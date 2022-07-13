ba57be2a9692586fdff3d528f87b373dcfa9c0e5
EZEN-130 <EZEN-130@DESKTOP-GD0BG1V>
2022-07-13 15:58:18 +0900

    Classmatename

---
+++ KJS/src/Classmatename.java
@@ -0,0 +1,10 @@
+
+public class Classmatename {
+	public static void main(String[] args) {
+		System.out.println("ABG, OGW, LDH");
+		System.out.println("PYM, CEJ, LJH");
+		System.out.println("NGH, SDY, KSJ");
+		System.out.println("KMJ, CYS, CHS");
+		System.out.println("WWJ, LYB, KTH");
+	}
+}
